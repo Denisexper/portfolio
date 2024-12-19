9ada93348765cb95a5068096997df87d2258289d
Denisexper <clasic.exper2023@gmail.com>
2024-12-19 16:20:48 -0600

    fix: fixed the about in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import About from "./components/about.jsx";
+import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
 import Experience from "./components/Experience.jsx";
 import Hero from "./components/Hero.jsx";
